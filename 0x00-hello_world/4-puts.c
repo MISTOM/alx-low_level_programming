32df247655e0f53ca197bc62d246943a971973e3
MISTOM <kigardetom2001@gmail.com>
2021-06-17 09:58:06 -0700

    use puts function as std output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char sentence[50];
+
+	strcpy(sentence, "\"Programming is like building a multilingual puzzle");
+	puts(sentence);
+	return (0);
+}
