4533a4f41962390657f478d19430c9dc0c592caf
MISTOM <kigardetom2001@gmail.com>
2021-06-17 11:07:27 -0700

    print sizes of types to std output

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Print size of various types on
+ * the computer it is compiled and run on.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char:%lu byte(s)\n", sizeof(char));
+	printf("Size of a int:%lu byte(s)\n", sizeof(int));
+	printf("Size of a long int:%lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int:%lu byte(s)\n", sizeof(long long int));
+
+	return (0);
+}
