6096629644b4011b8ffaefb0a57f01ec32188f1c
MISTOM <kigardetom2001@gmail.com>
2021-06-17 12:37:12 -0700

    add desctiption

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - entry point of the function
+ * Description: Using puts function to print
+ * to the standard output
+ * Return: 0 (success)
+ */
+
 int main(void)
 {
 	char sentence[52];
