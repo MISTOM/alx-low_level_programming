a53339391724ad41bbd40b8b4bcb27b23e2a3542
MISTOM <kigardetom2001@gmail.com>
2021-06-24 13:13:17 -0700

    add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: value to be checked
+ * Return: 1 if it is a digit
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 58);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - function that multiplies 2 numbers
+ * @a: first num
+ * @b: second num
+ * Return: product of @a and @b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
