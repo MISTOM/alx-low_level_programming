82f03047b8c1bebe0f70aca637adf692a7130dbd
MISTOM <kigardetom2001@gmail.com>
2021-06-24 13:34:00 -0700

    add4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - function that prints 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if(i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
