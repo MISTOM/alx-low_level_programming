92285fda4c1903fcd13b0fbca0296f4943503ef1
MISTOM <kigardetom2001@gmail.com>
2021-06-24 13:04:44 -0700

    add 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks if its capital letter
+ * @c: character to be checked
+ * Return: 1 if its capital
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
