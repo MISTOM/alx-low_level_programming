e3a1130ecb43645d61e32e84d37f4e21965441dc
MISTOM <kigardetom2001@gmail.com>
2021-06-24 13:41:17 -0700

    use continue

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
-
 /**
- * print_numbers - function that prints 0 to 9
- * Return: void
+ * print_most_numbers - print 0-9 excluding 2 and 4
  */
-
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = 48; i < 58; i++)
-	{
-		if (i != 50 && i != 52)
-		{
-			_putchar(i);
-		}
-	}
-	_putchar('\n');
+for (i = 48; i < 58; i++)
+{
+if (i == 50 || i == 52)
+{
+continue;
+}
+else
+{
+_putchar(i);
+}
+}
+_putchar('\n');
 }
