bc53e806f0616cfbd1b2652d7d7e9bc6e4ac9593
MISTOM <kigardetom2001@gmail.com>
2021-06-28 13:24:18 -0700

    add 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - resets the value to 98
+ * @n: pointer to the variable passed as a memory address
+ * to the variable to be reset.
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -4,4 +4,6 @@
 /* 1-swap.c */
 void swap_int(int *a, int *b);
 
+/* 0-reset_to_98.c */
+void reset_to_98(int *n);
 #endif
