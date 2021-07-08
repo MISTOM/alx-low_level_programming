4cc754ca4fa03d40feed66e7635effaeee68773d
MISTOM <kigardetom2001@gmail.com>
2021-07-08 13:52:17 -0700

    add prototypes to header file

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -2,7 +2,18 @@
 #define HOLBERTON_H
 int _putchar(char c);
 
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
 
 void _puts_recursion(char *s);
+
+int _sqrt_recursion(int n);
+
+int is_prime_number(int n);
 
 #endif
