90cc9092a7255bb3b162407567213fe5faee9c73
MISTOM <kigardetom2001@gmail.com>
2021-07-12 07:41:21 -0700

    add README.md and 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - entry point to the programm
+ * Description: Prints its name to the std output
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+## Argc Argv
+Passing arguments to the main function using *argc* and *argv*
+*By Holberton School of Software Engineering*
