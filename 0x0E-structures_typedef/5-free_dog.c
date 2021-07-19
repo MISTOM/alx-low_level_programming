24b6d4d7722849b41d6a8cd5ea29a6ed60747027
MISTOM <kigardetom2001@gmail.com>
2021-07-19 11:56:51 -0700

    modify sequence in which they are freed

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,14 +8,12 @@
 
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		free(d);
-		free(d->name);
-		free(d->owner);
-	}
-	else
+	if (!d)
 	{
 		return;
 	}
+
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
