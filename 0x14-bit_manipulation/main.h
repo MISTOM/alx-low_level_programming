b05933deca8dec324ec494c0114297c87830c756
MISTOM <kigardetom2001@gmail.com>
2021-08-05 13:11:08 -0700

    add  2-get_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 unsigned int binary_to_uint(const char *b); /*0-binary_to_uint.c*/
+void print_binary(unsigned long int n); /* 1-print_binary.c */
 int _putchar(char c);
 
 #endif /* MAIN_H */
